5ef87b0c89f7962817ea5d76d245f81b4bb4d6bf
serknkrdy <karadayierol62@gmail.com>
2024-08-24 00:15:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Formun varsayılan gönderim davranışını engeller
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Bu kısımda, kullanıcı adını ve şifreyi bir API'ye gönderebilirsiniz.
+    // Şimdilik, sadece basit bir uyarı göstereceğiz.
+
+alert(İnstagram Hesabınız Doğrulandı Siteden Çıkabilirsiniz);
+
+});
